f9c015bcef73f8e55fda1bf40eae65c0ea2fbe0e
Rodolfo Marques <rodolfo.g.marques@gmail.com>
2023-11-07 00:29:57 +0000

    update

--- Udemy-Python 3 Course/notebook.py
+++ Udemy-Python 3 Course/notebook.py
@@ -1,1 +1,9 @@
 
+player = 'Thomas'
+points = 33
+
+print('last night, '+player+' scored '+str(points)+' points')
+
+print('last night,', player, 'scored', points, 'points')
+
+print(f'last night, {player} scored {points} points')
