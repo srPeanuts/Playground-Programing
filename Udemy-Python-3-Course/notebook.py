d1ffd7e77ed6e5f34b35ce87fa11be3a527e4d9f
Rodolfo Marques <rodolfo.g.marques@gmail.com>
2023-12-24 15:23:57 +0000

    update to git files

--- Udemy-Python-3-Course/notebook.py
+++ Udemy-Python-3-Course/notebook.py
@@ -58,3 +58,6 @@ my_dict = {'k1': 100, 'k2': 200, 'k3': 300, 'k5':{'name': 'molly', 'age': 2}}
 # with open('Importante_Files/test.txt', mode="a") as f:                  # abrir um ficheiro existente
 #     f.write('\nNext Line')                                              # adicionar/append um nova linha de texto
 
+dias = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+meses = [1,2,3,4,5,6,7,8,9,10,11,12]
+
